41a613f1fe181ba8a0bf8a48ca21f1622a368f03
DESKTOP-3JEROE2\ginvi <ginvidi@gmail.com>
2017-04-04 18:23:24 +0100

    tidy up code

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const http = require('http');
 const fs = require('fs');
-
-
 const port = 8080;
 
 const server = http.createServer(listener);
 
 function listener(request, response) {
   response.writeHead(200, { 'Content-Type': 'text/html' });
-  fs.readFile("index.html", (err, data) => {
+  fs.readFile('index.html', (err, data) => {
 
     if(err) throw err;
     response.write(data);
